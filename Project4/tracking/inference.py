e7f5f40d21fd288fd381345eaf3ae9961f3d21d1
Greg Rosich <pyroscout14@gmail.com>
2019-04-12 15:26:18 -0600

    all working

--- Project4/tracking/inference.py
+++ Project4/tracking/inference.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -156,11 +156,13 @@ class ExactInference(InferenceModule):
         # Replace this code with a correct observation update
         # Be sure to handle the "jail" edge case where the ghost is eaten
         # and noisyDistance is None
+
         allPossible = util.Counter()
         if noisyDistance is None:
             jail_loc = self.getJailPosition()
             for p in self.legalPositions:
                 allPossible[p] = 0
+            allPossible[jail_loc] = 1.0
         else:
             for p in self.legalPositions:
                 trueDistance = util.manhattanDistance(p, pacmanPosition)
